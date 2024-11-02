ea11486f364250d23d09bae16342d299c4e49140
quambles <cpeter1492@gmail.com>
2024-11-02 13:54:56 -0600

    Initial submission no time complexity

--- code.js
+++ code.js
@@ -1,3 +1,13 @@
 function insertionSortReverse(array) {
-    return array;
+  for (var i = array.length - 2; i >= 0; i--)
+  {
+    var val = array[i];
+    var j;
+    for(j = i; j < array.length && array[j + 1] < val; j++)
+    {
+      array[j] = array[j + 1];
+    }
+      array[j] = val;
+  }  
+  return array;
 }
